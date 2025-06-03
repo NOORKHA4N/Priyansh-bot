e56b7c758940272783a0d55f337157ae1e003eac
NOORKHA4N <noorkhaan506@gmail.com>
2025-06-04 02:03:28 +0300

    Update 0_SAMEER-JUICES.js

--- Priyansh/commands/0_SAMEER-JUICES.js
+++ Priyansh/commands/0_SAMEER-JUICES.js
@@ -14,8 +14,8 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
         var { threadID, messageID } = event;
         if (event.body.indexOf("juice")==0 || event.body.indexOf("juices")==0 || event.body.indexOf("Juice")==0 || event.body.indexOf("JUICE")==0) {
                 var msg = {
-                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻  𝐒𝐚𝐦𝐞𝐞𝐫 𝐊𝐇𝐚𝐧 \n< ────────────────── >\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐉𝐮𝐢𝐂𝐞\n< ────────────────── >",
-                                attachment: fs.createReadStream(__dirname + `/noprefix/ab4938d3a1fc88ab1ce09a12124aa0dd.jpg`)
+                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻  𝐒𝐀𝐦𝐞𝐞𝐫 𝐊𝐡𝐀𝐚𝐧 \n< ────────────────── >\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐉𝐮𝐢𝐂𝐞\n< ────────────────── >",
+                                attachment: fs.createReadStream(__dirname + `/noprefix/98a697fd2edc0ce8a2e097125c324edf.jpg`)
                         }
                         api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("🍺", event.messageID, (err) => {}, true)
